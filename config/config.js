6b2bb59191220b6806e77da77a4a640cdf8f6bdd
Bryan <bryanarenas58@gmail.com>
2022-04-15 21:25:35 -0500

    Cambiar credenciales de base de datos Hosting

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 const databaseConfig = {
-    'host': '127.0.0.1',
+    'host': 'ec2-52-54-212-232.compute-1.amazonaws.com',
     'port': 5432,
-    'database': 'delivery_db',
-    'user': 'postgres',
-    'password': '12345678'
+    'database': 'd7mf9gfdhdmkga',
+    'user': 'jcvrpwtiwmibqc',
+    'password': '820572a84307c77f365b447ff492b1ec0016089f6619522e253e8cb010aacea0'
 };
 
 const db = pgp(databaseConfig);
